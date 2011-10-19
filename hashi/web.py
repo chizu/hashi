816a1b816299f685441bcc0c2720f68e1df98c4f
Nell Hardcastle <chizu@spicious.com>
2011-10-18 21:01:35 -0700

    Create a master class that sets everything up and registers clients as they're created.

--- hashi/web.py
+++ hashi/web.py
@@ -10,9 +10,10 @@ from zope.interface import implements
 class Hashioki(Resource):
     isLeaf = True
 
-    def __init__(self, nick):
+    def __init__(self, nick, client):
         Resource.__init__(self)
         self.nick = nick
+        self.client = client
         
     def render_GET(self, request):
         return """<html><p>If it existed, this would display the irc client for '{0}'.
@@ -21,15 +22,19 @@ class Hashioki(Resource):
 class HashiUserRealm(object):
     implements(IRealm)
 
+    def __init__(self, irc_clients):
+        self.irc_clients = irc_clients
+
     def requestAvatar(self, avatarId, mind, *interfaces):
         if IResource in interfaces:
             return (IResource,
-                    Hashioki(avatarId),
+                    Hashioki(avatarId, self.irc_clients[avatarId]),
                     lambda: None)
         raise NotImplementedError()
 
-def start():
-    portal = Portal(HashiUserRealm(), [FilePasswordDB('httpd.password')])
+def start(irc_clients):
+    portal = Portal(HashiUserRealm(irc_clients), 
+                    [FilePasswordDB('httpd.password')])
     credentialFactory = DigestCredentialFactory("md5", "localhost:8080")
     resource = HTTPAuthSessionWrapper(portal, [credentialFactory])
 
