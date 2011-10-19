f655292548bb39e148d4e9f73ae50c4d75c732f3
Nell Hardcastle <chizu@spicious.com>
2011-10-18 22:09:26 -0700

    Start on the JSON output for the client.

--- hashi/web.py
+++ hashi/web.py
@@ -1,38 +1,63 @@
+import json
+
 from twisted.cred.portal import IRealm, Portal
 from twisted.cred.checkers import FilePasswordDB
 from twisted.internet import reactor
 from twisted.web import server
 from twisted.web.guard import HTTPAuthSessionWrapper, DigestCredentialFactory
 from twisted.web.resource import Resource, IResource
+from twisted.web.rewrite import RewriterResource
 from zope.interface import implements
 
 
 class Hashioki(Resource):
     isLeaf = True
 
-    def __init__(self, nick, client):
-        Resource.__init__(self)
-        self.nick = nick
-        self.client = client
-        
+    def getChild(self, name, request):
+        if name == '':
+            return self
+        return Resource.getChild(self, name, request)
+
     def render_GET(self, request):
-        return """<html><p>If it existed, this would display the irc client for '{0}'.</p></html>""".format(self.nick)
+        return """<html><p>If it existed, this would display the irc client for '{0}'.</p></html>""".format(request.irc_nick)
+
+
+class Channel(Resource):
+    isLeaf = True
+
+    def render_GET(self, request):
+        return json.dumps(request.irc_client.channels)
+
+
+def irc_rewriter(avatarId, client):
+    def func(request):
+        request.irc_nick = avatarId
+        request.irc_client = client
+    return func
+
 
 class HashiUserRealm(object):
     implements(IRealm)
 
-    def __init__(self, irc_clients):
+    def __init__(self, irc_clients, rest_api):
         self.irc_clients = irc_clients
+        self.rest_api = rest_api
 
     def requestAvatar(self, avatarId, mind, *interfaces):
         if IResource in interfaces:
+            client = self.irc_clients[avatarId]
+            rewrite = RewriterResource(self.rest_api, 
+                                       irc_rewriter(avatarId, client))
             return (IResource,
-                    Hashioki(avatarId, self.irc_clients[avatarId]),
+                    rewrite,
                     lambda: None)
         raise NotImplementedError()
 
 def start(irc_clients):
-    portal = Portal(HashiUserRealm(irc_clients), 
+    rest_api = Hashioki()
+    rest_api.putChild('channel', Channel())
+
+    portal = Portal(HashiUserRealm(irc_clients, rest_api), 
                     [FilePasswordDB('httpd.password')])
     credentialFactory = DigestCredentialFactory("md5", "localhost:8080")
     resource = HTTPAuthSessionWrapper(portal, [credentialFactory])
