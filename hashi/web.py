3b022671a09cf45b2c149365a2fa8bce05aba8ca
Nell Hardcastle <chizu@spicious.com>
2011-10-18 19:43:01 -0700

    Basic HTTP auth for now.

--- hashi/web.py
+++ hashi/web.py
@@ -1,10 +1,15 @@
+from twisted.cred.portal import IRealm, Portal
+from twisted.cred.checkers import FilePasswordDB
 from twisted.internet import reactor
-from twisted.web.resource import Resource
 from twisted.web import server
+from twisted.web.guard import HTTPAuthSessionWrapper, DigestCredentialFactory
+from twisted.web.resource import Resource, IResource
+from zope.interface import implements
 
 
 class Hashioki(Resource):
     isLeaf = True
+
     def __init__(self, clients=None):
         Resource.__init__(self)
         self.clients = clients
@@ -13,9 +18,23 @@ class Hashioki(Resource):
         return """<html><p>If it existed, this would display the irc client.
 </p></html>"""
 
+class HashiUserRealm(object):
+    implements(IRealm)
+
+    def requestAvatar(self, avatarId, mind, *interfaces):
+        if IResource in interfaces:
+            return (IResource,
+                    Hashioki(),
+                    lambda: None)
+        raise NotImplementedError()
 
 def start():
-    root = Hashioki()
-    site = server.Site(root)
+    portal = Portal(HashiUserRealm(), [FilePasswordDB('httpd.password')])
+    credentialFactory = DigestCredentialFactory("md5", "localhost:8080")
+    resource = HTTPAuthSessionWrapper(portal, [credentialFactory])
+
+    site = server.Site(resource)
+
     reactor.listenTCP(8080, site)
+
     return site
