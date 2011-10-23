56d2af8c57832e85e972747154303309e5723a9e
Nell Hardcastle <chizu@spicious.com>
2011-10-23 13:46:27 -0700

    CSS, some template markup for testing things.

--- static/client.js
+++ static/client.js
@@ -1,15 +1,20 @@
 $(document).ready(function() {
+	$("#content").tabs();
 	base = "/api/networks";
 	$.getJSON(base, function(networks) {
 		network_count = networks.length;
 		for (var n=0; n < network_count; n++) {
-			$("#root").append("<div id=\"" + networks[n] + "\"/>");
+			var n_id = networks[n];
+			$("#root").append("<div id=\"" + n_id + "\"/>");
+			$("#"+n_id).addClass("network");
 			$.getJSON(base + "/" + networks[n], function (n_id) {
 				return function(channels) {
+					$(n_id).append("<ul></ul>");
+					$(n_id + " ul").addClass("channels");
 					$.map(channels, function (c) {
-						$(n_id).append("<li>"+c+"</li>");
+						$(n_id + " ul").append("<li>"+c+"</li>");
 					});
-				}}("#"+networks[n]));
+				}}("#"+n_id));
 		}
 	});
 });
