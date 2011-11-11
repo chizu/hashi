8ad3e52d66953f11e0eb91b9911593a113514264
Nell Hardcastle <chizu@spicious.com>
2011-11-10 21:26:35 -0800

    Unicode safety and send server name with events.

--- hashi/history.py
+++ hashi/history.py
@@ -2,16 +2,8 @@
 import time
 from collections import namedtuple
 
-import redis
+import psycopg2
 import zmq
-
-class Identity(object):
-    def __init__(self, redis, group, token):
-        self.redis = redis
-        set_name = "identity:{group}".format(group=group)
-        self.redis.sadd(set_name, token)
-        self.token = token
-
 
 Event = namedtuple('Event', ['kind', 'source', 'target', 'content',
                              'timestamp'])
@@ -20,44 +12,8 @@ Event = namedtuple('Event', ['kind', 'source', 'target', 'content',
 class History(object):
     """Interact with client history for a network."""
     def __init__(self, irc_network):
-        self.redis = redis.Redis()
+        self.conn = psycopg2.connect("dbname=hashi")
         self.irc_network = irc_network
-
-    def identity(self, token):
-        return Identity(self.redis, self.irc_network, token)
-
-    def add_event(self, identity, event):
-        # Identity is the client that submitted this event
-        event_id = self.new_event(event)
-        self.observe_event(identity, event_id)
-        return event_id
-
-    def new_event(self, event):
-        event_id = self.redis.incr("event.next")
-        event_key = "event:{id}".format(id=event_id)
-        self.redis.hmset(event_key, 
-                         {"kind": event.kind,
-                          "source": event.source.token,
-                          "target": event.target.token,
-                          "content": event.content,
-                          "timestamp": event.timestamp})
-        return event_id
-
-    def get_event(self, event_id):
-        event_key = "event:{id}".format(id=event_id)
-        values = self.redis.hvals(event_key)
-        return Event(*values)
-
-    def get_observed_history(self, identity, length=40):
-        key = "observed:{group}:{identity}".format(group=self.irc_network,
-                                                   identity=identity.token)
-        return (self.get_event(x) for x in self.redis.lrange(key, -40, -1))
-
-    def observe_event(self, identity, event_id):
-        key = "observed:{group}:{identity}".format(group=self.irc_network,
-                                                   identity=identity.token)
-        self.redis.rpush(key, event_id)
-
 
 class RemoteEventReceiver(object):
     def __init__(self):
@@ -76,9 +32,10 @@ class RemoteEventReceiver(object):
 
             # New history
             if self.socket in socks and socks[self.socket] == zmq.POLLIN:
-                event = self.socket.recv_multipart()
+                event = [unicode(x) for x in self.socket.recv_multipart()]
                 identity, kind = event[:2]
                 print("{0}:{1}:{2}".format(identity, kind, event[2:]))
+                
 
             # Queries against the history
             if self.queries in socks and socks[self.queries] == zmq.POLLIN:
