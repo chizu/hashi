8653434aa9d941e4e21b498e0ac74429197065df
Nell Hardcastle <chizu@spicious.com>
2011-11-20 20:30:12 -0800

    Rewrite history to use PostgreSQL directly.

--- hashi/history.py
+++ hashi/history.py
@@ -5,15 +5,93 @@ from collections import namedtuple
 import psycopg2
 import zmq
 
+
+psycopg2.extensions.register_type(psycopg2.extensions.UNICODE)
+psycopg2.extensions.register_type(psycopg2.extensions.UNICODEARRAY)
+
+
 Event = namedtuple('Event', ['kind', 'source', 'target', 'content',
                              'timestamp'])
+
+history_registry = dict()
+identity_registry = dict()
+
+
+class Identity(object):
+    def __new__(cls, history, token):
+        # Pull it out of cached objects if we can
+        if token in identity_registry:
+            return identity_registry[token]
+        else:
+            id_obj = object.__new__(cls, history, token)
+            identity_registry[token] = id_obj
+            return id_obj
+
+    def __init__(self, history, token):
+        self.history = history
+        self.sql = history.sql
+        self.token = token
+        cur = self.sql.cursor()
+        def get_identity():
+            cur.execute("SELECT id FROM identities WHERE token = %s", (token,))
+            return cur.fetchone()
+        result = get_identity()
+        if result:
+            self.id = result[0]
+        else:
+            cur.execute("INSERT INTO identities (token) VALUES (%s)",
+                        (token,))
+            self.id = get_identity()[0]
+        self.sql.commit()
+        identity_registry[self.token] = self
+
+
+class NickIdentity(Identity):
+    def __new__(cls, history, token):
+        token = token.split("!")[0]
+        # After modifying the token, do exactly as Identity
+        return Identity.__new__(NickIdentity, history, token)
 
 
 class History(object):
     """Interact with client history for a network."""
+    def __new__(cls, irc_network):
+        # Pull it out of cached objects if we can
+        if irc_network in history_registry:
+            return history_registry[irc_network]
+        else:
+            h_obj = object.__new__(cls, irc_network)
+            history_registry[irc_network] = h_obj
+            return h_obj
+
     def __init__(self, irc_network):
-        self.conn = psycopg2.connect("dbname=hashi")
+        self.sql = psycopg2.connect("dbname=hashi")
         self.irc_network = irc_network
+        cur = self.sql.cursor()
+        cur.execute("SELECT id FROM networks WHERE name = %s", (irc_network,))
+        result = cur.fetchone()
+        if result:
+            self.id = result[0]
+        else:
+            cur.execute("INSERT INTO networks (name) VALUES (%s)",
+                        (irc_network,))
+        # Done with setup, commit
+        self.sql.commit()
+
+    def record(self, identity, kind, args):
+        cur = self.sql.cursor()
+        record_sql = """INSERT INTO events (network_id, source, target, args)
+VALUES (%s, %s, %s, %s);"""
+        # Record each kind of message, with a fallback for unimplemented ones
+        if kind == 'privmsg':
+            source = NickIdentity(self, args[0]).id
+            target = Identity(self, args[1]).id
+            cur.execute(record_sql, (self.id, source, target, args[2:]))
+        else:
+            # No formatter, stuff it all into the args column (to prevent loss)
+            cur.execute(record_sql, (self.id, None, None, args))
+        self.sql.commit()
+
 
 class RemoteEventReceiver(object):
     def __init__(self):
@@ -33,9 +111,13 @@ class RemoteEventReceiver(object):
             # New history
             if self.socket in socks and socks[self.socket] == zmq.POLLIN:
                 event = [unicode(x) for x in self.socket.recv_multipart()]
-                identity, kind = event[:2]
-                print("{0}:{1}:{2}".format(identity, kind, event[2:]))
-                
+                network, identity, kind = event[:3]
+                args = event[3:]
+                history = History(network)
+                id_obj = NickIdentity(history, identity)
+                history_registry[network].record(id_obj, kind, args)
+                print("{0}:{1}:{2}:{3}".format(network, id_obj.token, kind, 
+                                               args))
 
             # Queries against the history
             if self.queries in socks and socks[self.queries] == zmq.POLLIN:
