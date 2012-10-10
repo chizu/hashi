c1a1d2c93bd93a15b4ec933709dfc4094eba1438
Nell Hardcastle <chizu@spicious.com>
2012-10-10 19:26:06 +0000

    Begin reworking sidebars for user/channel lists for mobile.

--- static/client.js
+++ static/client.js
@@ -122,7 +122,7 @@ function serverControls(hostname) {
     hostname_id = hostnameId(hostname);
     modal_id = hostname_id + '-channel-join';
     modal_url = '../api/networks/'+hostname;
-    return '<div class="subnav subnav-fixed"><ul class="nav nav-pills channels-nav"><li><a class="btn-primary" data-toggle="modal" href="#'+modal_id+'"><i class="icon-plus icon-white"/></a></li></ul></div><div class="tab-content"></div><div id="'+modal_id+'" class="modal fade hide"><div class="modal-header">Open a tab!<a class="close" data-dismiss="modal"><i class="icon-remove" /></a></div><div class="modal-body"><form class="form-inline"><input type="text" class="input channel-name" placeholder="Channel or User" /> <input type="text" class="input channel-key" placeholder="Key" /></form></div><div class="modal-footer"><a href="'+modal_url+'" class="btn btn-primary">Open</a></div></div>';
+    return '<div class="subnav panel"><ul class="nav nav-list channels-nav"><li><a class="btn-primary" data-toggle="modal" href="#'+modal_id+'"><i class="icon-plus icon-white"/></a></li></ul></div><div class="tab-content panel"></div><div id="'+modal_id+'" class="modal fade hide"><div class="modal-header">Open a tab!<a class="close" data-dismiss="modal"><i class="icon-remove" /></a></div><div class="modal-body"><form class="form-inline"><input type="text" class="input channel-name" placeholder="Channel or User" /> <input type="text" class="input channel-key" placeholder="Key" /></form></div><div class="modal-footer"><a href="'+modal_url+'" class="btn btn-primary">Open</a></div></div>';
 }
 
 function handleEvent(event) {
@@ -281,17 +281,10 @@ function addChannelTab(hostname, channel) {
 	li.append(link);
 	$('#'+hostname_id).find('.channels-nav').append(li);
 	
-	$('#'+hostname_id).children('.tab-content')
-	    .append('<div id="'+channel_id+'" class="tab-pane"><table class="irc-body"></table></div>');
+	server_children = $('#'+hostname_id).children('.tab-content');
+	server_children.append('<div id="'+channel_id+'" class="tab-pane active"><table class="irc-body"></table><ul class="users-nav panel"></ul></div>');
 	$(eid(channel_id)).append('<form><input class="channel-input" id="'+channel_id+'-input" name="'+channel+'" size="16" type="text" /></form>');
 	$(eid(channel_id)).children('form').submit(options, channelInput);
-	users_handle = $(document.createElement('div'))
-	users_handle.addClass('left-grab');
-	users_handle.addClass('btn').addClass('btn-info');
-	users_handle.append('<i class="icon-chevron-left icon-white"></i>');
-	popover_options = {title: 'Users', content: function () {return displayUserList(hostname, channel)}, placement: 'left'};
-	users_handle.popover(popover_options);
-	$(eid(channel_id)).append(users_handle);
     }
 }
 
